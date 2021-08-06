d3b19865b94007e1e304adc427ced578e206f2b3
ARYANAMBATKAR <88045544+ARYANAMBATKAR@users.noreply.github.com>
2021-08-06 16:04:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,100 @@
+var bow , arrow,  background;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+
+function preload(){
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  green_balloonImage = loadImage("blue_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+  
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
+  
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+  
+}
+
+function draw() {
+ background(0);
+  // moving ground
+    scene.velocityX = -3 ;
+  
+ 
+  if (frameCount%100===0){
+      arrow = createSprite (380,bow.y,10,10);
+      arrow.addImage(arrowImage);
+      arrow.scale=0.30;
+    arrow.velocityX=-3
+    arrow.lifetime=200;
+      }
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+  //moving bow
+  bow.y = World.mouseY
+  
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+    
+  }
+  
+  //creating continous balloons
+  var select_balloon = Math.round(random(1,1));
+  
+  if (World.frameCount % 100 == 0) {
+    if (select_balloon == 1) {
+      Balloon();
+    }
+  }
+  
+  drawSprites();
+}
+
+
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+}
+
+
+function  Balloon() {
+  var  balloon = createSprite(0,Math.round(random(20, 370)), 100,100);
+  balloon .velocityX = 3;
+   balloon.lifetime = 150;
+   balloon.scale = 0.1;
+ 
+    var balloon0 = Math.round(random(1,2));
+      switch(balloon0){
+          case 1:balloon.addImage(red_balloonImage);
+          break;
+          case 2:balloon.addImage(green_balloonImage);
+          default: break;  
+
+}
+
+}   
